e205b7aef64613117db6061be5c23bbe4b3c9a5c
ivan-venegas-avila <ivan.venegas41@gmail.com>
2022-05-26 20:32:02 -0400

    clasen4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   get '/tv_shows', to: 'tv_shows#index'
-
-  root 'tv_shows#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/welcome', to: 'pages#welcome'
+  root 'pages#welcome'
 end
